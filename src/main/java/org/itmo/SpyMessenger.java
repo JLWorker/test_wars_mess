f2547f95c6fc7ea316a04781c5a5edebc9253c9c
Makar <makar@MacBook-Pro-Makar.local>
2025-03-18 15:19:41 +0300

    sd

--- src/main/java/org/itmo/SpyMessenger.java
+++ src/main/java/org/itmo/SpyMessenger.java
@@ -1,11 +1,66 @@
 package org.itmo;
 
-class SpyMessenger {
-    void sendMessage(String sender, String receiver, String message, String passcode) {
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+class SpyMessenger {
+    class Message {
+        public String sender;
+        public String receiver;
+        public String message;
+        public String passcode;
+        public LocalDateTime sendTime;
+        public boolean forceDelete;
+    };
+
+    Map<String, List<Message>> inbox; // index from receiver -> [Message]
+
+    void sendMessage(String sender, String receiver, String message, String passcode) {
+        Message m = new Message();
+        m.sender = sender;
+        m.receiver = receiver;
+        m.message = message;
+        m.passcode = passcode;
+        m.forceDelete = false;
+        m.sendTime = LocalDateTime.now();
+
+        List<Message> ms = inbox.get(receiver);
+        if (ms == null) {
+            List<Message> newQueue = new ArrayList<>();
+            newQueue.add(m);
+            inbox.put(receiver, newQueue);
+        }
+        else {
+            if (ms.size() == 5) {
+                ms.remove(0);
+            }
+            ms.add(m);
+        }
     }
 
     String readMessage(String user, String passcode) {
-        return "Top Secret";
+        List<Message> ms = inbox.get(user);
+        if (ms == null) {
+            return "";
+        }
+
+        for (int i = 0; i < ms.size(); i++) {
+            Message m = ms.get(i);
+            if (m.passcode.equals(passcode)) {
+                if (m.forceDelete || ChronoUnit.MILLIS.between(m.sendTime, LocalDateTime.now()) > 1500) {
+                    m.forceDelete = true;
+                    return "";
+                }
+                else {
+                    m.forceDelete = true;
+                    return m.message;
+                }
+            }
+        }
+
+        return "";
     }
 }
